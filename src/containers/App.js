5362a7b394bdc760408292dfa1261f9277419ee3
nikmargit <nikola.margit@gmail.com>
2018-04-18 00:11:47 +0200

    add router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch
+} from "react-router-dom";
 
 import Login from "./login";
+import Home from "./home";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to
-                    reload.
-                </p>
-                <Login />
-            </div>
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/login" component={Login} />
+                    </Switch>
+                </div>
+            </Router>
         );
     }
 }
