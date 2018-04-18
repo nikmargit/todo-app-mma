229ecde2f1f4404ccd07750bf58b961dd2ebeef2
anchi84 <anarili@gmail.com>
2018-04-18 18:32:43 +0200

    ADD fetch for task list

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,8 +1,49 @@
 import React from "react";
 
 class Home extends React.Component {
+    
+    constructor(props) {
+            super(props);
+            this.state = {
+                todos: []
+            }
+    }
+
+    componentDidMount() {
+        const url = `https://jsonplaceholder.typicode.com/todos`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                this.setState({todos: data});
+            });
+    }
+
     render() {
-        return <h1>Home!!!</h1>;
+        return (
+            <div>
+                <h1>Home!!!</h1>
+                {
+                    this.state.todos.map(
+                        (todo, index) => 
+                            <div key={index}>
+                                <p>{todo.title}</p>
+                                <input type="checkbox"/>
+                                <select>
+                                    {
+                                        this.state.todos.map(
+                                            todo => {
+                                               <option key={todo.title}>{todo.userId}</option> 
+                                            }
+                                        )
+                                    }
+                                </select>
+                            </div>
+                    )
+                }
+            </div>
+        ) 
+        
     }
 }
 
