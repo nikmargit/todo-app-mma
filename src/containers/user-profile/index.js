42408ddc631950f6fe911ca3f91af718dd410664
ml250271 <lukicmilica@hotmail.com>
2018-04-18 15:09:50 +0200

    ADD fetch for UserProfiles

--- src/containers/user-profile/index.js
+++ src/containers/user-profile/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 class UserProfile extends React.Component {
+    componentDidMount() {
+        const url = `https://jsonplaceholder.typicode.com/users`;
+        fetch(url)
+            .then(resp => resp.json())
+            .then(function(data) {
+                console.log(data);
+            });
+    }
     render() {
         return <h1>User Profile!!!</h1>;
     }
