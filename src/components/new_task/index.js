f56dd73b12063fd453a21a3c9165775712793f2e
nikmargit <nikola.margit@gmail.com>
2018-04-26 18:33:16 +0200

    add remove task button

--- src/components/new_task/index.js
+++ src/components/new_task/index.js
@@ -1,41 +1,37 @@
 import React from "react";
 
 class NewTask extends React.Component {
-    
+    addNewTask(node) {}
 
-    addNewTask(node) {
-
-    }
-  
     render() {
-        return (
-            this.props.users ? (
+        return this.props.users ? (
             <div style={{ margin: 10, clear: "both" }}>
                 <hr />
-                <input
-                    name="completed"
-                    type="checkbox"
+                <input name="completed" type="checkbox" />
+                <textarea
+                    ref={node => {
+                        this.addNewTask(node);
+                    }}
                 />
-                <textarea ref={node => {
-                    this.addNewTask(node);
-                }} />
                 <select
                     name="userId"
                     // value={this.props.task.userId}
                     // onChange={event => this.updateTask(event)}
                 >
-                {this.props.users.map(user => (
-                    <option defaultValue="" value={user.id}>{user.name}</option>
-                ))}
+                    {this.props.users.map(user => (
+                        <option defaultValue="" value={user.id}>
+                            {user.name}
+                        </option>
+                    ))}
                 </select>
-          </div>
-          
-          ) : (
+                <button onClick={event => this.addNewTask(event)}>
+                    Add new task!
+                </button>
+            </div>
+        ) : (
             <p />
-            )
-            
-        )
+        );
     }
-  }
+}
 
-  export default NewTask;
+export default NewTask;
