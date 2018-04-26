0533fa9a19a597703e3a1c37a6d675bef7cc131c
ml250271 <you@example.com>
2018-04-26 19:41:33 +0200

    add header component and index file

---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Admin!</h1>
+                <button>Sign out</button>
+                <hr />
+            </div>
+        );
+    }
+}
+
+export default Header;
