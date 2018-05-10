ed1829fe389b84a8be95f6e76b86b531b208154d
nikola <nikola.margit@gmail.com>
2018-05-10 13:09:19 +0200

    change css user-profile

--- src/containers/user-profile/index.js
+++ src/containers/user-profile/index.js
@@ -24,23 +24,27 @@ class UserProfile extends React.Component {
 
         return user ? (
             <div>
-                <h1>Name: {user.name}</h1>
-                <p>Email: {user.email}</p>
-                <p>User Name: {user.username}</p>
-                <p>Company: {user.company.name}</p>
-                <p>Phone: {user.phone}</p>
-                <p>Tasks:</p>
-                <ul>
-                    {this.props.todos
-                        .filter(
-                            todo =>
-                                todo.userId * 1 === user.id &&
-                                todo.completed === false
-                        )
-                        .map(todo => {
-                            return <li key={todo.id}>- {todo.title}</li>;
-                        })}
-                </ul>
+                <div className="pop-info">
+                    <h1>{user.name}</h1>
+                    <p className="pop-company">{user.company.name}</p>
+                    <p className="pop-email">Email: {user.email}</p>
+                    <p className="pop-user-name">User Name: {user.username}</p>
+                    <p className="pop-phone">Phone: {user.phone}</p>
+                </div>
+                <div className="pop-tasks-container">
+                    <p className="pop-tasks">Tasks:</p>
+                    <ul>
+                        {this.props.todos
+                            .filter(
+                                todo =>
+                                    todo.userId * 1 === user.id &&
+                                    todo.completed === false
+                            )
+                            .map(todo => {
+                                return <li key={todo.id}>- {todo.title}</li>;
+                            })}
+                    </ul>
+                </div>
                 <button onClick={() => this.props.resetId()}>Back</button>
             </div>
         ) : (
